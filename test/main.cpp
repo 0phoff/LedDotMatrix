d6deeb408c54d75ed1816ef08c0310040bae695d
0phoff <0phoff@users.noreply.github.com>
2020-01-02 20:51:17 +0100

    Updated test file to show single led test

--- test/main.cpp
+++ test/main.cpp
@@ -1,16 +1,35 @@
 #include <iostream>
 #include <unistd.h>
-
 #include "matrix.h"
+
+#define NUM_CASCADED 4
 
 int main()
 {
   std::cout << "--- LED DOT MATRIX ---" << std::endl;
 
   // Create matrix of 4 cascaded devices on spidev0.0
-  LDM::Matrix mat("/dev/spidev0.0", 4);
-  sleep(2);
+  LDM::Matrix mat("/dev/spidev0.0", NUM_CASCADED);
+  sleep(1);
 
+  // Set single led and flush to display
+  std::cout << "  Single led test" << std::endl;
   mat.setLed(10, 4, true);
   mat.flush();
+  sleep(1);
+
+  // Flush without clearing buffer
+  std::cout << "  Flush without clearing test" << std::endl;
+  for (int i=0; i < 8*NUM_CASCADED; i++) {
+    for (int j=0; j < 8; j++) {
+      mat.setLed(i, j, true);
+    }
+    mat.flush(false);
+    sleep(0.5);
+  }
+  sleep(1);
+
+  // Clear matrix
+  std::cout << "  Clearing matrix test" << std::endl;
+  mat.clear();
 }
